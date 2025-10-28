4bcbd8cabbf6f7f52baca81bd041785688ecbaaf
Seziberagabriel <g.tuyisingi@alustudent.com>
2025-10-28 20:57:54 +0200

    initial commit

---
+++ nutribridge-backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { pool } from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('NutriBridge API running'));
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, async () => {
+  try {
+    await pool.connect();
+    console.log(`✅ Database connected`);
+  } catch (err) {
+    console.error('❌ Database connection failed', err);
+  }
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
