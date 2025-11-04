b43a141263ba94b48dc325cc37d06a5fef42803a
Seziberagabriel <g.tuyisingi@alustudent.com>
2025-11-04 19:09:10 +0200

    initial commit

--- nutribridge-frontend/src/api/api.js
+++ nutribridge-frontend/src/api/api.js
@@ -2,23 +2,15 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'http://localhost:5000/api',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('token')}`
-  }
+  headers: { 'Content-Type': 'application/json' },
 });
 
-// Example: get user info
-export const getUser = async (id) => {
-  const res = await API.get(`/users/${id}`);
-  return res.data;
+export const loginUser = async (email, password) => {
+  try {
+    const res = await API.post('/auth/login', { email, password });
+    return res.data;
+  } catch (err) {
+    throw err.response?.data?.message || 'Login failed';
+  }
 };
-
-// Example: update user info
-export const updateUser = async (id, data) => {
-  const res = await API.put(`/users/${id}`, data);
-  return res.data;
-};
-
-// You can add more API functions here, e.g., login, register, etc.
 
